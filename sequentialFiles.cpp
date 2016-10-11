1efe98615712768e024aa19ceffd6940ea5c3306
Ishan Joshi <ishanjoshi02@gmail.com>
2016-10-11 15:18:57 +0530

    Update sequentialFiles.cpp

--- sequentialFiles.cpp
+++ sequentialFiles.cpp
@@ -1,129 +1,239 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <string.h>
 using namespace std;
-class Student {
-public:
-	string name,rollNumber,contactAddress;
-};
-class Files : protected  Student {
-	fstream file;
-	Student s[15];
-	int numberOfStudent;
-	char nameOfFile[20] = "Ishan.txt";
+class Error {
+	int code;
+	string description;
 public :
-	void create();
+	Error(int a, string b) {
+		code = a;
+		description = b;
+	}
+	void displayError() {
+		cout << "Error " << code << endl
+				<< description << endl;
+	}
+};
+class Database {
+	char nameOfFile[21];
+	ofstream ofile;
+	ifstream ifile;
+	int numberOfStudents;
+public:
+	void getData();
 	void display();
-	void insert();
+	void addRecord();
 	void deleteRecord();
 	void modifyRecord();
-	void removeFile();
+	void searchRecord();
+	void deleteFile();
 };
-void Files::removeFile() {
-	remove(nameOfFile);
-}
-void Files::create() {
-	cout << "Enter the Number of Student in Database\n";
-	cin >> numberOfStudent;
-	file.open(nameOfFile,ios::out|ios::in);
-	for(int i=0;i<numberOfStudent;i++) {
-		cout << "Enter the Name, Roll Number and Contact Address of Student " << i + 1
-				<< endl ;
-		cin >> s[i].name >> s[i].rollNumber >> s[i].contactAddress ;
-		file << s[i].rollNumber <<endl<< s[i].name << endl <<s[i].contactAddress << endl;
-	}
-	file.close();
-}
-void Files::display() {
-	file.open(nameOfFile,ios::out|ios::in);
-	for(int i=0;i<numberOfStudent;i++) {
-		file >> s[i].rollNumber >> s[i].name >> s[i].contactAddress;
-		cout << "Name : " << s[i].name << endl
-				<< "Roll Number : " << s[i].rollNumber << endl
-				<< "Contact Address : " << s[i].contactAddress << endl;
-	}
-	file.close();
-}
-void Files::insert() {
-	file.open(nameOfFile,ios::app|ios::out);
-	numberOfStudent++;
-	int i = numberOfStudent - 1;
-	cout << "Enter the Name, Roll Number and Contact Address of Student " << numberOfStudent
-			<< endl ;
-	cin >> s[i].name >> s[i].rollNumber >> s[i].contactAddress ;
-	file << s[i].rollNumber <<endl << s[i].name <<endl<<s[i].contactAddress << endl;
-	file.close();
-}
-void Files::modifyRecord() {
-	string answer;
-	cout << "Enter the Roll Number of Student\n";
-	cin >> answer;
-	for(int i=0;i<numberOfStudent;i++) {
-		file >> s[i].rollNumber;
-		if(s[i].rollNumber == answer) {
-
-			cout << "Enter the Name, Roll Number and Contact Address of Student " << i + 1
-					<< endl ;
-			cin >> s[i].name >> s[i].rollNumber >> s[i].contactAddress ;
-			file << s[i].rollNumber <<endl<< s[i].name << endl <<s[i].contactAddress << endl;
-			break;
+void Database::getData() {
+	string a,b;
+	cout << "Enter the Name of File\n";
+	cin >> nameOfFile;
+	strcat(nameOfFile,".dat");
+	ofile.open(nameOfFile,ios::binary);
+	try{
+		if(ofile.eof()) {
+			throw Error (404,"File Not Found\n");
+		}
+		else {
+			string onceMore;
+			do {
+				cout << "Enter the Name of Student followed by his/her Roll Number\n";
+				cin >> a >> b;
+				ofile << a << endl
+						<< b << endl;
+				cout << "Do you want to Enter again\n";
+				cin >> onceMore;
+			}while((onceMore == "Yes" || onceMore == "yes"
+					|| onceMore == "y" || onceMore == "Y")
+					&& numberOfStudents++);
 		}
 	}
-	file.close();
+	catch (Error &e) {
+		e.displayError();
+	}
 }
-void Files::deleteRecord() {
-	string answer;
-	fstream temp;
-	cout << "Enter the Roll Number of Student\n";
-	cin >> answer;
-	file.open(nameOfFile);
-	temp.open("temp.txt");
-	for(int i = 0; i < numberOfStudent ;i++) {
-		if(s[i].rollNumber != answer) {
-			temp << s[i].rollNumber <<endl<< s[i].name << endl <<s[i].contactAddress << endl;
+void Database::display() {
+	int i;
+	string a,b;
+	ifile.open(nameOfFile,ios::binary);
+	try {
+		if(ifile.eof()) {
+			throw Error(123,"No such File exists\n"
+					"Please create a File first\n");
+			for(i=1;i<=numberOfStudents;i++) {
+				ifile >> a >> b;
+				cout << "Name : " << i << " is " << a << endl
+						<< "Roll Number : " << b << endl;
+			}
 		}
 	}
-	file.close();
+	catch(Error &r) {
+		r.displayError();
+		this->getData();
+	}
+}
+void Database::addRecord() {
+	string a,b;
+	ofile.open(nameOfFile,ios::binary);
+	try{
+		if(ofile.eof()) {
+			throw Error (404,"File Not Found\n");
+		}
+		else {
+			string onceMore;
+			do {
+				cout << "Enter the Name of Student followed by his/her Roll Number\n";
+				cin >> a >> b;
+				ofile << a << endl
+						<< b << endl;
+				cout << "Do you want to Enter again\n";
+				cin >> onceMore;
+			}while((onceMore == "Yes" || onceMore == "yes"
+					|| onceMore == "y" || onceMore == "Y")
+					&& numberOfStudents++);
+		}
+	}
+	catch (Error &e) {
+		e.displayError();
+	}
+}
+void Database::deleteRecord() {
+	string a,b,searcher;
+	cout << "Enter the Name of Student or Roll Number of Student\n";
+	cin >> searcher;
+	ofile.open("temp.dat",ios::binary);
+	ifile.open(nameOfFile,ios::binary);
+	try {
+		if(ifile.eof() || ofile.eof()) {
+			throw Error(456,"No such file exists\nPlease create Database first\n");
+		}
+		else {
+			for(int i = 0;i<numberOfStudents;i++) {
+				ifile >> a >> b;
+				if(a!=searcher && b != searcher) {
+					ofile << a << endl << b << endl;
+				}
+			}
+		}
+		numberOfStudents --;
+		remove(nameOfFile);
+		rename("temp.dat",nameOfFile);
+	}
+	catch (Error &o) {
+		o.displayError();
+		this->getData();
+	}
+}
+void Database::modifyRecord() {
+	string a,b,searcher;
+	cout << "Enter the Name of Student or Roll Number of Student\n";
+	cin >> searcher;
+	ofile.open("temp.dat",ios::binary);
+	ifile.open(nameOfFile,ios::binary);
+	try {
+		if(ifile.eof() || ofile.eof()) {
+			throw Error(456,"No such file exists\nPlease create Database first\n");
+		}
+		else {
+			for(int i = 0;i<numberOfStudents;i++) {
+				ifile >> a >> b;
+				if(a!=searcher && b != searcher) {
+					ofile << a << endl << b << endl;
+				}
+				else {
+					cout << "Enter the Name and Roll Number of Student\n";
+					cin >> a >> b;
+					ofile << a << endl << b << endl;
+				}
+			}
+		}
+			remove(nameOfFile);
+			rename("temp.dat",nameOfFile);
+		}
+		catch (Error &o) {
+			o.displayError();
+			this->getData();
+		}
+}
+void Database::searchRecord() {
+	string a,b,searcher;
+		cout << "Enter the Name of Student or Roll Number of Student\n";
+		cin >> searcher;
+		ifile.open(nameOfFile,ios::binary);
+		try {
+			if(ifile.eof()) {
+				throw Error(456,"No such file exists\nPlease create Database first\n");
+			}
+			else {
+				for(int i = 0;i<numberOfStudents;i++) {
+					ifile >> a >> b;
+					if(a==searcher && b == searcher) {
+						cout << a << endl << b << endl;
+					}
+				}
+			}
+		}
+		catch (Error &o) {
+			o.displayError();
+			this->getData();
+		}
+}
+void Database::deleteFile() {
 	remove(nameOfFile);
-	rename("temp.txt",nameOfFile);
 }
 int main(int argc, char **argv) {
-	Files fp;
-	int opt;
+	Database dat;
+	string saveFile;
+	int option;
 	do {
-		cout << "1. Create a Database\n"
-				<< "2. Display the Database\n"
-				<< "3. Add a Record\n"
-				<< "4. Delete a Record\n"
-				<< "5. Modify a Record\n"
-				<< "6. Exit\n";
-		cin >> opt;
-		switch(opt) {
+		cout << "1. Create Database\n"
+				"2. Display Database\n"
+				"3. Add a Record\n"
+				"4. Delete a Record\n"
+				"5. Modify a Record\n"
+				"6. Search a Record\n"
+				"7. Exit\n";
+		cin >> option;
+		switch(option) {
 		case 1 : {
-			fp.create();
+			dat.getData();
 			break;
 		}
 		case 2 : {
-			fp.display();
+			dat.display();
 			break;
 		}
 		case 3 : {
-			fp.insert();
+			dat.addRecord();
 			break;
 		}
 		case 4 : {
-			fp.deleteRecord();
+			dat.deleteRecord();
 			break;
 		}
 		case 5 : {
-			fp.modifyRecord();
+			dat.modifyRecord();
 			break;
+		}
+		case 6 : {
+			dat.searchRecord();
+			break;
+		}
+		case 7 : {
+			cout << "Do you want to save the file\n";
+			cin >> saveFile;
+			if(saveFile == "y" || saveFile == "Y"
+					|| saveFile == "yes" || saveFile == "Yes") {
+				dat.deleteFile();
+			}
+			return 0;
 		}
 		}
 	}while(true);
-	cout << "Do you want to save the File?\n1.Yes\n2.No\n";
-	cin >> opt;
-	if(opt == 2) {
-		fp.removeFile();
-	}
 }
